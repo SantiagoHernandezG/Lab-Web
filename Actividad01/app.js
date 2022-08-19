30f81933938bb1f98da554e365887e1100c9a1c3
SantiagoHernandezG <Santiagohernandez2@icloud.com>
2022-08-19 10:51:24 -0500

    Finished calculator

--- Actividad01/app.js
+++ Actividad01/app.js
@@ -1,14 +1,49 @@
 const calculadora = require('./mate')
+const prompt = require('prompt-sync')();
 
-let res = calculadora.suma(1,3)
+let opp = 1
+salir:
+while(opp !=6){
+    console.log("********************")
+    console.log("*    CALCULADORA   *")
+    console.log("*                  *")
+    console.log("*  1. Sumar        *")
+    console.log("*  2. Restar       *")
+    console.log("*  3. Multiplicar  *")
+    console.log("*  4. Dividir      *")
+    console.log("*  5. Modulo       *")
+    console.log("*  6. Salir        *")
+    console.log("*                  *")
+    console.log("********************")
+    opp = prompt('Elija una opción: ')
+    console.log('* * * * * * * * * * * * *');
 
-console.log("********************")
-console.log("*    CALCULADORA   *")
-console.log("*                  *")
-console.log("*  1. Sumar        *")
-console.log("*  2. Restar       *")
-console.log("*  3. Multiplicar  *")
-console.log("*  4. Dividir      *")
-console.log("*  5. Modulo       *")
-console.log("*                  *")
-console.log("********************")
+    if(opp == 6){
+        console.log("Saliendo...")
+        break salir;
+    }
+
+    const num1 = prompt('Primer número: ')
+    const num2 = prompt('Segundo número: ')
+
+    switch (opp) {
+        case "1":
+            console.log('Resultado: '+calculadora.suma(parseInt(num1),parseInt(num2)))
+            break;
+        case "2":
+            console.log('Resultado: '+calculadora.resta(parseInt(num1),parseInt(num2)))
+            break;
+        case "3":
+            console.log('Resultado: '+calculadora.multiplicacion(parseInt(num1),parseInt(num2)))
+            break;
+        case "4":
+            console.log('Resultado: '+calculadora.division(parseInt(num1),parseInt(num2)))
+            break;
+        case "5":
+            console.log('Resultado: '+calculadora.modulo(parseInt(num1),parseInt(num2)))
+            break;
+        default:
+            console.log('Opcion invalida');
+            break salir;
+    }
+}
--- Actividad01/mate.js
+++ Actividad01/mate.js
@@ -1,7 +1,6 @@
 exports.suma = function(a,b){
     return a+b;
 };
-
 exports.resta = function(a,b){
     return a-b;
 };
