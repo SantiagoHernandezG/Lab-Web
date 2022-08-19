a90f563e957b1a91d27e70cf3bc0a6bef0df03a3
SantiagoHernandezG <Santiagohernandez2@icloud.com>
2022-08-19 10:06:51 -0500

    Calculator basic logic

---
+++ Actividad01/app.js
@@ -0,0 +1,14 @@
+const calculadora = require('./mate')
+
+let res = calculadora.suma(1,3)
+
+console.log("********************")
+console.log("*    CALCULADORA   *")
+console.log("*                  *")
+console.log("*  1. Sumar        *")
+console.log("*  2. Restar       *")
+console.log("*  3. Multiplicar  *")
+console.log("*  4. Dividir      *")
+console.log("*  5. Modulo       *")
+console.log("*                  *")
+console.log("********************")
